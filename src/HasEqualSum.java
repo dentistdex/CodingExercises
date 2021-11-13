0238adb597ebdbf1f2cb9a30afa56404c5556193
Semih Çalışkan <dtsemihcaliskan@gmail.com>
2021-11-13 22:31:38 +0300

    ,,,

---
+++ src/HasEqualSum.java
@@ -0,0 +1,26 @@
+public class HasEqualSum {
+    public static boolean hasEqualSum (int first,int second, int fİnal) {
+        if (first + second == fİnal){
+            return true;
+        }else {
+            return false;}
+    }
+
+    public static boolean hasTeen (int a , int b , int c) {
+        if ((a >= 13 && a <= 19) || (b >= 13 && b <=19) || (c >= 13 && c <=19)) {
+            return true;}
+        else {
+            return false;}
+    }
+
+    public static boolean isTeen (int d) {
+        if (d <= 19 && d >= 13) {
+            return true;
+        }else { return false;}
+    }
+}
+
+
+
+
+
