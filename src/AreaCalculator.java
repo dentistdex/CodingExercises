3c4d916b8fa9964c6666b78f1802297ed2fbb52d
Semih Çalışkan <dtsemihcaliskan@gmail.com>
2021-11-14 01:03:43 +0300

    ,,,

---
+++ src/AreaCalculator.java
@@ -0,0 +1,19 @@
+public class AreaCalculator {
+
+    public static double area (double radius) {
+        if (radius < 0 ){
+            System.out.println("Invalid Value");
+            return -1;
+        }else {
+            return (double) radius * radius * Math.PI;
+        }
+    }
+    public static double area (double x, double y) {
+        if (x < 0 || y < 0){
+            System.out.println("Invalid Value");
+            return -1;
+        }else {
+            return (double) x * y;
+        }
+    }
+}
